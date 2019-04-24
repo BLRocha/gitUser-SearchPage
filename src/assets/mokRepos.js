3bfdf62a19a806d57254ecb650fa374a972eb538
l217 <leandrorocha.cbrj@hotmail.com>
2019-04-23 23:47:54 -0300

    update mok add 2 repos per line and 3 users per line

--- src/assets/mokRepos.js
+++ src/assets/mokRepos.js
@@ -1,6 +1,24 @@
 module.exports = [
   {
-  id: 148730060,
+  id: 14873550060,
+  node_id: "MDEwOlJlcG9zaXRvcnk=",
+  name: "NO REPOS",
+  full_name: "NO REPO",
+  private: false,
+  owner: {
+  login: "user git",
+  id: 34800137,
+  node_id: "MDQ6VXNlcjM0ODAwMTM3",
+  avatar_url: "avatar.jpg",
+  gravatar_id: "",
+  url: "#",
+  html_url: "#",
+  type: "User",
+  site_admin: false
+  }
+},
+{
+  id: 14873440060,
   node_id: "MDEwOlJlcG9zaXRvcnk=",
   name: "NO REPOS",
   full_name: "NO REPO",
--- src/assets/mokStarred.js
+++ src/assets/mokStarred.js
@@ -18,5 +18,25 @@ module.exports = [
   type: "User",
   site_admin: false
   }
+},
+{
+  id: 1812602056,
+  node_id: "MDEwOlJlccnkxODE2MDIwNTY=",
+  name: "NO USER",
+  full_name: "NO USER",
+  private: false,
+  owner: {
+  login: "NO USER",
+  id: 44379716,
+  node_id: "MDQ6lcjQ0Mzc5NzE2",
+  avatar_url: "avatar.jpg",
+  gravatar_id: "",
+  url: "#",
+  html_url: "#",
+  followers_url: "#",
+  following_url: "#",
+  type: "User",
+  site_admin: false
+  }
 }
   ]
